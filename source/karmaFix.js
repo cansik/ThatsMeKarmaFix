982a6d2167c740962b557bae37ba00094fa0832e
Florian <florian@nexpose.ch>
2015-03-24 10:22:22 +0100

    version 1 implemented

---
+++ source/karmaFix.js
@@ -0,0 +1,8 @@
+//Extension Content Code
+//----------------------
+var karmas = document.getElementsByClassName('karma-plus');
+
+for(var i = 0; i < karmas.length; i++)
+{
+    karmas[i].innerHTML = '+&infin;';
+}
